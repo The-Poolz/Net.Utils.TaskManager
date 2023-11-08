cc57203b9a0591a6ef7c090ed31ab5640105a107
RoyKodman <roykod18@gmail.com>
2023-11-08 22:04:41 +0100

    Add the code

---
+++ TaskManager/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskManager;
+
+internal class Program
+{
+    internal static Random random = new();
+    static async Task Main(string[] args)
+    {
+        TaskManager manager = new TaskManager(); // Initialize with an empty collection if needed
+
+        // Add tasks
+        for (int i = 0; i < 10; i++)
+        {
+            manager.AddTask(GetTask(i, 0, manager));
+        }
+
+        // Start and wait for all tasks to complete
+        await manager.StartAsync();
+
+        Console.WriteLine("All tasks have been processed.");
+    }
+
+    public static Task GetTask(int i, int page, IAddTask addTask)
+    {
+        return new Task(() =>
+        {
+            Console.WriteLine($"Starting task {i}, page {page}");
+            var result = random.NextDouble(); // Simulate work
+            Thread.Sleep((int)(result * 10000)); // Simulate asynchronous work
+            var gotMore = result < 0.5;
+            if (gotMore) //this is the "GotMore" on the result
+                addTask.AddTask(GetTask(i, page + 1, addTask));
+            Console.WriteLine($"Finished task {i}, page {page}, gotMore {gotMore}");
+        });
+    }
+}
---
+++ TaskManager/TaskManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager;
+
+public class TaskManager : IAddTask
+{
+    private readonly ConcurrentBag<Task> tasks = new ConcurrentBag<Task>();
+    private bool started = false;
+
+    public void AddTask(Task task)
+    {
+        tasks.Add(task);
+        if (started) task.Start();
+    }
+
+    public async Task StartAsync()
+    {
+        if (started)
+            throw new InvalidOperationException("Cannot start the task manager twice");
+        started = true;
+        Parallel.ForEach(tasks, (task) => { task.Start(); });
+        await AwaitFinish();
+    }
+
+    public bool IsCompleted => tasks.All(t => t.IsCompleted);
+    public async Task AwaitFinish()
+    {
+        while (!IsCompleted)
+        {
+            await Task.WhenAll(tasks);
+        }
+    }
+}
