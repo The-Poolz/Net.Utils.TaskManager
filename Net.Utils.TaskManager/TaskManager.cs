95b3a49c6d4f0a0b49092a47f572da7403df413a
RoyKodman <roykod18@gmail.com>
2023-11-09 11:52:29 +0100

    - add project - add .sln

---
+++ Net.Utils.TaskManager/TaskManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+
+namespace Net.Utils.TaskManager;
+
+public class TaskManager
+{
+    private readonly ConcurrentBag<Task> _tasks;
+    private bool started;
+    public bool IsCompleted => _tasks.All(t => t.IsCompleted);
+
+    public TaskManager()
+    {
+        _tasks = new ConcurrentBag<Task>();
+    }
+
+    public TaskManager(IEnumerable<Task> tasks) : this()
+    {
+        AddRange(tasks);
+    }
+
+    public void Add(Task task)
+    {
+        _tasks.Add(task);
+        if (started) task.Start();
+    }
+
+    public void AddRange(IEnumerable<Task> tasks)
+    {
+        tasks.ToList().ForEach(Add);
+    }
+
+    public async Task StartAsync()
+    {
+        if (started) throw new InvalidOperationException("Cannot start the task manager twice");
+        started = true;
+        Parallel.ForEach(_tasks, task => { task.Start(); });
+        await AwaitFinish();
+    }
+
+    private async Task AwaitFinish()
+    {
+        while (!IsCompleted)
+        {
+            await Task.WhenAll(_tasks);
+        }
+    }
+}
